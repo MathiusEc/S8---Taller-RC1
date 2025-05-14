37f5972211efe898aa6637beaf4310ef13f363f0
Nicolas Acosta <emilio.acosta@udla.edu.ec>
2025-05-13 21:12:56 -0500

    13/05/2025 Upgrade Menu

--- funciones.c
+++ funciones.c
@@ -5,9 +5,10 @@
 int menu(){
     int opc;
     printf("-------MENU-------\n");
-    printf("1.Ingresar producto\n");
-    printf("2.Editar producto\n");
-    printf("3.\n");
+    printf("1.Ingresar producto.\n");
+    printf("2.Editar producto.\n");
+    printf("3.Eliminar producto.\n");
+    printf("4.Salir del programa.\n");
     printf("-----FIN MENU-----\n");
    return opc;
 }
