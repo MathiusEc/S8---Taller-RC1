35ea7b150f4f1a7e9e17c2b7d15fe7d28531c3b4
Nicolas Acosta <emilio.acosta@udla.edu.ec>
2025-05-13 21:06:28 -0500

    13/05/2025

---
+++ funciones.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "funciones.h"
+
+int menu(){
+    int opc;
+    printf("-------MENU-------\n");
+    printf("1.Ingresar producto\n");
+    printf("2.Editar producto\n");
+    printf("3.\n");
+    printf("-----FIN MENU-----\n");
+   return opc;
+}
